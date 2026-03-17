876e8531ffac59779789f6c2ff0ad55463f257d2
samsulali2 <samsul.ali.officebackup@gmail.com>
2026-03-17 15:22:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def analyze_news(text):
     """
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+     model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}]
     )
 
